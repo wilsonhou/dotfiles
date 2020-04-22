1423837727bb98fed8e3808e0b841aae94d13e38
Wilson <wilshou@gmail.com>
2020-04-23 09:38:45 +1000

    updated configs

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,6 +8,15 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
+# Check if external monitor is attached
+my_laptop_external_monitor=$(xrandr --query | grep 'HDMI1')
+
+# Launch bar1 and (bar2 if external is attached)
 polybar mybar -c ~/.config/polybar/config.ini &
-polybar mybar2 -c ~/.config/polybar/config.ini &
+
+if [[ $my_laptop_external_monitor = *disconnected* ]]; then
+	echo "1 Polybar launched..."
+else
+	polybar mybar2 -c ~/.config/polybar/config.ini &
+	echo "2 Polybars launched..."
+fi
